3175e9812ff761d8d8b5bbb79ca8d6cd45ea963e
Your Name <you@example.com>
2015-12-14 16:47:29 +0100

    program with sparse output

--- program1.cpp
+++ program1.cpp
@@ -256,4 +256,96 @@ for (auto i = it.begin(); i != it.end(); ++i){
     cout<< *i <<" ";
 }
 cout<<endl;
+cout << "Begin iterations" << endl;
+	changes1=1;
+	changes2=1;
+	flag=0;
+	int l(0);
+	int k(0);
+	for (auto cont = it.begin(); cont != it.end(); cont++){
+	while(k<*cont && changes1+changes2-changes1*changes2==1){
+		if(k%2==0){
+		changes1=0;
+		for(int j=0;j<N;j++){
+			l = C[j].get_length();
+			if(l==1 && l<M){
+				if(!R[(C[j][0]+1)%M].search(j)){
+					C[j][0]=(C[j][0]+1)%M;
+					if(changes1==0)
+						changes1=1;
+				};
+			};
+			if(l>1){
+				flag=C[j][0]==0 && C[j][l-1]==M-1;
+				for(int i=0;i<l-flag;i++){
+					if(mod_dist(C[j][i+1],C[j][i],M)>1 && !R[(C[j][i]+1)%M].search(j)){
+						C[j][i]=(C[j][i]+1)%M;
+						if(changes1==0)
+							changes1=1;
+						if(C[j][i]==0)
+							C[j].last_first();
+					};
+				};
+			};
+		};
+		};
+		if(k%2==1){
+			changes2=0;
+		for(int i=0;i<M;i++){
+			l = R[i].get_length();
+			if(l==1 && l<N){
+				if(!C[(R[i][0]+1)%N].search(i)){
+					R[i][0]=(R[i][0]+1)%N;
+					if(changes2==0)
+						changes2=1;
+				};
+			};
+			if(l>1){
+				flag=R[i][0]==0 && R[i][l-1]==N-1;
+				for(int j=0;j<l-flag;j++){
+					if(mod_dist(R[i][j+1],R[i][j],N)>1 && !C[(R[i][j]+1)%N].search(i)){
+						R[i][j]=(R[i][j]+1)%N;
+						if(changes2==0)
+							changes2=1;
+						if(R[i][j]==0)
+							R[i].last_first();
+					};
+				};
+			};
+		};
+		};
+		k++;
+	};
+	ofstream out2;
+    out2.open("stepsparse"+to_string(*cont)+".csv");
+    int lr;
+	for(int i=0;i<M;i++){
+		lr = R[i].get_length();
+		if(lr==0){
+			for(int j=0;j<N;j++)
+				out2 << to_string(C[j].search(i)) << ",";
+		};
+		if(lr==1){
+			for(int j=0;j<R[i][0];j++)
+				out2 << to_string(C[j].search(i))<<",";
+            out2 << to_string(2)<<",";
+			for(int j=R[i][0]+1;j<N;j++)
+				out2 << to_string(C[j].search(i))<<",";
+		}
+		else{
+			for(int j=0;j<R[i][0];j++)
+				out2 << to_string(C[j].search(i))<<",";
+			for(int r=0;r<lr;r++){
+				out2<< to_string(2)<<",";
+				for(int j=R[i][r]+1;j<R[i][r+1];j++)
+					out2 << to_string(C[j].search(i))<<",";
+			};
+			for(int j=R[i][lr-1]+1;j<N;j++)
+					out2 << to_string(C[j].search(i))<<",";
+			};
+        out2 << endl;
+        };
+    out2.close();
+    }
 }
+
