8e5f74caacca97e80bbc14ac5f153d4488b7cad4
Santo Gioia <orgsg1993@hotmail.com>
2015-12-18 17:25:01 +0100

    Intestazione classe structure. Un insulto all'intelligenza umana, c'è solo la dichiarazione degli attributi, si deve studiare il constructor

---
+++ struct.h
@@ -0,0 +1,7 @@
+class structure {
+    vect<vect<unsigned short int> R;
+    vect<vect<unsigned short int> C;
+    public:
+        structure( vect<vect<unsigned short int> R, vect<vect<unsigned short int> C) {
+        };
+};
