0b44dc7e5cce21a37bccfac0159f33f43011ac82
Santo Gioia <orgsg1993@hotmail.com>
2015-12-19 12:03:18 +0100

    First version of the constructor for the data structure.

--- struct.h
+++ struct.h
@@ -1,7 +1,27 @@
+#include<stdlib.h>
+#include<iostream>
+#include<memory>
+#include<algorithm>
+#include<ostream>
+#include<fstream>
+#include<math.h>
+#include "clvect.h"
+
+using namespace std;
+
 class structure {
-    vect<vect<unsigned short int> R;
-    vect<vect<unsigned short int> C;
+    vect<vect<unsigned short int>> R;
+    vect<vect<unsigned short int>> C;
+
     public:
-        structure( vect<vect<unsigned short int> R, vect<vect<unsigned short int> C) {
+        structure( vect<vect<unsigned short int>> R, vect<vect<unsigned short int>> C ) {
+            unsigned int m( R.get_length() );
+            for (int i=0; i<m; i++){
+                structure::R.tail_add(R[i]);
+            }
+            unsigned int n( C.get_length() );
+            for (int i=0; i<m; i++){
+                structure::C.tail_add(C[i]);
+            }
         };
 };
